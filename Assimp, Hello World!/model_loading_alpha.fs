f7e791c74bbab2f47d0c744dd0a1f111c1be3e95
MarioBros2699 <105845978+MarioBros2699@users.noreply.github.com>
2023-06-07 00:24:24 +0200

    Modified model_loading_alpha.fs
    Cut over fragment migliorato

--- Assimp, Hello World!/model_loading_alpha.fs
+++ Assimp, Hello World!/model_loading_alpha.fs
@@ -54,47 +54,28 @@ void main()
 {   
  
     vec4 color = texture(texture_diffuse1, fs_in.TexCoords);
-    vec3 normal = normalize(fs_in.Normal);
-    vec3 lightColor = vec3(1.0);
-    // ambient
-    vec3 ambient = 0.5 * color.rgb;
-    // diffuse
-    vec3 lightDir = normalize(lightPos - fs_in.FragPos);
-    float diff = max(dot(lightDir, normal), 0.0);
-    vec3 diffuse = diff * lightColor;
-    // specular
-    vec3 viewDir = normalize(viewPos - fs_in.FragPos);
-    vec3 reflectDir = reflect(-lightDir, normal);
-    float spec = 0.0;
-    vec3 halfwayDir = normalize(lightDir + viewDir);  
-    spec = pow(max(dot(normal, halfwayDir), 0.0), 64.0);
-    vec3 specular = spec * lightColor;    
-    // calculate shadow
-    float shadow = ShadowCalculation(fs_in.FragPosLightSpace);                      
-    vec4 lighting = vec4((ambient + (1.0 - shadow) * (diffuse + specular)), 1.0) * color;    
+    if(color.a == 0.0){
+        discard;
+    }else{
+        vec3 normal = normalize(fs_in.Normal);
+        vec3 lightColor = vec3(1.0);
+        // ambient
+        vec3 ambient = 0.5 * color.rgb;
+        // diffuse
+        vec3 lightDir = normalize(lightPos - fs_in.FragPos);
+        float diff = max(dot(lightDir, normal), 0.0);
+        vec3 diffuse = diff * lightColor;
+        // specular
+        vec3 viewDir = normalize(viewPos - fs_in.FragPos);
+        vec3 reflectDir = reflect(-lightDir, normal);
+        float spec = 0.0;
+        vec3 halfwayDir = normalize(lightDir + viewDir);  
+        spec = pow(max(dot(normal, halfwayDir), 0.0), 64.0);
+        vec3 specular = spec * lightColor;    
+        // calculate shadow
+        float shadow = ShadowCalculation(fs_in.FragPosLightSpace);                      
+        vec4 lighting = vec4((ambient + (1.0 - shadow) * (diffuse + specular)), 1.0) * color;    
+        FragColor = lighting;
+    }
 
-    FragColor = lighting;
-    
-
-    // vec4 color = texture(texture_diffuse1, fs_in.TexCoords);
-    // vec3 normal = normalize(fs_in.Normal);
-    // vec3 lightColor = vec3(0.3);
-    // // ambient
-    // vec4 ambient = 0.3 * color;
-    // // diffuse
-    // vec3 lightDir = normalize(lightPos - fs_in.FragPos);
-    // float diff = max(dot(lightDir, normal), 0.0);
-    // vec4 diffuse = vec4(diff * lightColor, 1.0);
-    // // specular
-    // vec3 viewDir = normalize(viewPos - fs_in.FragPos);
-    // vec3 reflectDir = reflect(-lightDir, normal);
-    // float spec = 0.0;
-    // vec3 halfwayDir = normalize(lightDir + viewDir);  
-    // spec = pow(max(dot(normal, halfwayDir), 0.0), 64.0);
-    // vec4 specular = vec4(spec * lightColor, 1.0);    
-    // // calculate shadow
-    // float shadow = ShadowCalculation(fs_in.FragPosLightSpace);                      
-    // vec4 lighting = (ambient + (1.0 - shadow) * (diffuse + specular)) * color;    
-    
-    // FragColor = vec4(lighting);
 }
